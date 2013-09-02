e3122ddbac599f8697d2f31af3edc20f337a8c95
nekohage <satoshi.otsubo@gmail.com>
2013-09-02 11:04:07 +0900

    warプラグイン追加

--- playTest/project/Build.scala
+++ playTest/project/Build.scala
@@ -1,6 +1,7 @@
 import sbt._
 import Keys._
 import play.Project._
+import com.github.play2war.plugin._
 
 object ApplicationBuild extends Build {
 
@@ -14,8 +15,11 @@ object ApplicationBuild extends Build {
     javaEbean
   )
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
+  val main = play.Project(appName, appVersion, appDependencies)
     // Add your own project settings here      
+    .settings(Play2WarPlugin.play2WarSettings: _*)
+    .settings(                                       
+      Play2WarKeys.servletVersion := "3.0"
   )
 
 }
